17c83807930505f26dbb8b1e8cbde9f95c1384a6
hasti44 <hastijiyani04@gmail.com>
2024-07-14 19:03:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+// App.js
+ 
+import { Component } from 'react';
+import './App.css';
+import Game from './Game.js';
+ 
+class App extends Component{  
+    render(){
+      return (
+      <Game/>
+    );
+  }
+}
+ 
+export default App;
